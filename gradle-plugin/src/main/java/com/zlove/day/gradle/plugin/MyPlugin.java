f193c6530ce984ce98e044e048824c05c9853690
zlove.zhang <zlove.zhang@bytedance.com>
2021-01-19 09:55:15 +0800

    add plugin

--- gradle-plugin/src/main/java/com/zlove/day/gradle/plugin/MyPlugin.java
+++ gradle-plugin/src/main/java/com/zlove/day/gradle/plugin/MyPlugin.java
@@ -8,5 +8,6 @@ public class MyPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         System.out.println("apply my plugin");
+        project.getTasks().create("mytask", MyTask.class);
     }
 }
