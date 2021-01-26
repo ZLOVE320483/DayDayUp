d1e776da8a7d6608032a059ec33979ebb041eb02
zlove.zhang <zlove.zhang@bytedance.com>
2021-01-26 20:48:02 +0800

    plugin apk

---
+++ library/day-plugin/src/main/java/com/zlove/day/plugin/DayPluginActivity.kt
@@ -0,0 +1,16 @@
+package com.zlove.day.plugin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Author by zlove, Email zlove.zhang@bytedance.com, Date on 2021/1/26.
+ * PS: Not easy to write code, please indicate.
+ */
+class DayPluginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plugin)
+    }
+}
